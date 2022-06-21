58fed7bee2d39180727accb7052c2fc54434fca0
alantheandroid <alessio.ilardi@gmail.com>
2022-06-21 23:49:59 +0200

    feat: add promise chaining

--- exercise-4.js
+++ exercise-4.js
@@ -1,37 +1,45 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
 const jobs = [
   {
     id: 1,
-    jobTitle: 'CEO'
+    jobTitle: "CEO",
   },
   {
     id: 2,
-    jobTitle: 'Project Manager'
+    jobTitle: "Project Manager",
   },
   {
     id: 3,
-    jobTitle: 'Developer'
-  }
+    jobTitle: "Developer",
+  },
 ];
 
-// core here
+const fetchPersonById = (id) =>
+  new Promise((resolve) => resolve(persons.find((person) => person.id === id)));
+
+const fetchJobById = (person) =>
+  jobs.find((job) => job.id == person.id).jobTitle;
+
+fetchPersonById(1).then((person) =>
+  console.log(`${person.firstName} ${person.lastName}, ${fetchJobById(person)}`)
+);
